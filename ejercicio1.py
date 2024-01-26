b01942a3d31a38d87033b3eeac5d009a5bdd1fb4
Henry Andrade <henry.andrade@upb.edu.co>
2024-01-26 15:29:26 -0500

    version 2.1

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,3 +1,12 @@
 print("Curso de programación")
 for i in range(1000):
 	print("Hola")
+línea de código
+línea de código
+línea de código
+línea de código
+línea de código
+línea de código
+línea de código
+línea de código
+línea de código
